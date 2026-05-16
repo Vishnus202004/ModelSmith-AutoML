78040b865ce95ebc13746ee66048a8425cd800ca
Vishnu s <vishnus202004@gmail.com>
2026-05-16 11:48:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,21 @@
-from app.main import app
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api")
+def home():
+    return {
+        "message": "FastAPI running on Vercel 🚀"
+    }
+
+app.include_router(router)
